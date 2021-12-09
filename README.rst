26509b1ae657adeaa7366d2caea9035cdb9b1624
T. Franzel <tfranzel@users.noreply.github.com>
2021-12-10 00:09:59 +0100

    packaging

--- README.rst
+++ README.rst
@@ -1,6 +1,70 @@
+================
 django-seriously
-----------------
+================
+
+|pypi-version|
 
     ... wait what? no seriously, why isn't that part of Django/DRF?
 
-Opinionated collection of Django and DRF tools that came in handy time and again.
+Opinionated collection of `Django`_ and `Django REST framework`_ tools that came in handy time and again.
+
+- ``AdminItemAction``
+    - Allow triggering context-aware custom admin operations in model list views.
+
+- ``admin_navigation_link``
+    - Allow navigation from the admin list view to other related models via links.
+
+- ``MinimalUser`` (abstract model)
+    - Bare minimum user model ready for customization.
+    - Removes the username and auxiliary fields like `first_name` `last_name`.
+    - Allow creating users without a valid password (unusable password)
+    - Abstract since its highly recommended to subclass the user model anyway.
+
+- ``ValidatedJSONField`` (model field)
+    - validate the structure of JSON fields with Pydantic models.
+
+- ``TokenAuthentication``
+    - When OAuth2 adds too much complexity, DRF's TokenAuthentication is too simple, and
+      `django-rest-knox`_ does not quite fit the permissioning.
+    - No plain passwords in database (PBKDF2, i.e. hashed and salted)
+    - Enabled for permission scoping
+    - Easy token creation in Django admin
+
+- ``BaseModel`` (abstract model)
+    - Reusable base model with automatic ``created_at``, ``updated_at`` fields.
+    - Primary key is a random UUID (``uuid4``).
+    - Ensure validation logic (``full_clean()``) always runs, not just in a subset of cases.
+
+
+License
+-------
+
+Provided by `T. Franzel <https://github.com/tfranzel>`_, `Licensed under 3-Clause BSD <https://github.com/tfranzel/django-seriously/blob/master/LICENSE>`_.
+
+Requirements
+------------
+
+-  Python >= 3.6
+-  Django >= 3.0
+-  Django REST Framework (optional)
+
+Installation
+------------
+
+.. code:: bash
+
+    $ pip install django-seriously
+
+
+Usage
+-----
+
+TODO
+
+
+.. _Django: https://www.djangoproject.com/
+.. _Django REST framework: https://www.django-rest-framework.org/
+.. _django-rest-knox: https://github.com/James1345/django-rest-knox
+
+.. |pypi-version| image:: https://img.shields.io/pypi/v/django-seriously.svg
+   :target: https://pypi.python.org/pypi/django-seriously
---
+++ setup.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import shutil
+import sys
+
+from setuptools import setup
+
+name = "django-seriously"
+package = "django_seriously"
+description = (
+    "Opinionated collection of Django and DRF tools that came in handy time and again."
+)
+url = "https://github.com/tfranzel/django-seriously"
+author = "T. Franzel"
+author_email = "tfranzel@gmail.com"
+license = "BSD"
+
+
+with open("README.rst") as readme:
+    long_description = readme.read()
+
+with open("requirements/base.txt") as fh:
+    requirements = [r for r in fh.read().split("\n") if not r.startswith("#")]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, "__init__.py")).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(
+        1
+    )
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
+
+
+version = get_version(package)
+
+
+if sys.argv[-1] == "publish":
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit(1)
+    os.system("python setup.py sdist bdist_wheel")
+    if os.system("twine check dist/*"):
+        print("twine check failed. Packages might be outdated.")
+        print("Try using `pip install -U twine wheel`.\nExiting.")
+        sys.exit(1)
+    if os.system("twine upload dist/*"):
+        print("failed to upload package")
+        sys.exit(1)
+    if os.environ.get("CI"):
+        os.system("git config user.name github-actions")
+        os.system("git config user.email github-actions@github.com")
+    os.system(f"git tag -a {version} -m 'version {version}'")
+    if os.system("git push --tags"):
+        print("failed pushing release tag")
+        sys.exit(1)
+    shutil.rmtree("dist")
+    shutil.rmtree("build")
+    shutil.rmtree("django_seriously.egg-info")
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=version,
+    url=url,
+    license=license,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=requirements,
+    extras_require={},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    project_urls={
+        "Source": "https://github.com/tfranzel/django-seriously",
+    },
+)
