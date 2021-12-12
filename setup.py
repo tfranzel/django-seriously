f30ce25a00bf27e44d2312a13af2d14af84f1663
T. Franzel <tfranzel@users.noreply.github.com>
2021-12-12 12:28:06 +0100

    packaging

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
     install_requires=requirements,
     extras_require={},
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Django",
         "Framework :: Django :: 3.2",
@@ -103,6 +103,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Internet :: WWW/HTTP",
     ],
     project_urls={
